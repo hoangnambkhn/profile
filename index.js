ab341fa1f083233a5426fe5c4aa729196170e814
hoang nam <namhoang18595@gmail.com>
2017-08-26 14:35:18 +0000

    initial commit

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var port = process.env.PORT || 8080;
 
 var MongoClient = require("mongodb").MongoClient;
 var mongoose = require("mongoose");
-var client = require("./models/client");
+// var client = require("./models/client");
+var messageDb = require("./models/message");
 
 
 
@@ -18,15 +19,17 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
     // gửi trả kết quả khi request là http://127.0.0.1:port/home
-    var data = new client({
-        ip : req.header('x-forwarded-for') || req.connection.remoteAddress,
-        language : req.headers['accept-language'].split(',')[0],
-        systemdata : req.headers["user-agent"].match(/\((.*?)\)/)[1]
-    })
-    data.save(function(err){
-        if(err) console.err(err);
-        console.log("client save");
-    })
+    // var data = new client({
+    //     ip : req.header('x-forwarded-for') || req.connection.remoteAddress,
+    //     language : req.headers['accept-language'].split(',')[0],
+    //     systemdata : req.headers["user-agent"].match(/\((.*?)\)/)[1]
+    // })
+    // data.save(function(err){
+    //     if(err) console.err(err);
+    //     console.log("client save");
+    // })
+    
+    
     // var data = {}
     // data.ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
     // data.language = req.headers['accept-language'].split(',')[0];
@@ -77,7 +80,31 @@ app.get('/confes',function(req, res) {
     res.render('confes');
 })
 app.get('/confes/sendmessage',function(req, res) {
-    res.send("tks for question")
+    var nameReq = req.query.name;
+    if(!nameReq) {
+        nameReq = "Anonymous";
+    }
+    var messageReq = req.query.message;
+    var messageData = new messageDb({
+        name : nameReq,
+        message : messageReq
+    })
+    messageData.save(function(err){
+        if(err) console.error(err);
+    })
+    res.redirect('/confes/thankyou')
+})
+
+app.get('/admin/getconfesdata/01652577727',function(req, res) {
+    messageDb.find({},function(err,data){
+        if(err) console.error(err);
+        console.log(data);
+        res.json({"client" : data});
+        
+    })
+})
+app.get('/confes/thankyou',function(req, res) {
+    res.render("respone");
 })
 app.listen(port,function(err){
     if(err) console.error(err);
